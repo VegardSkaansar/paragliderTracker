6008109fcbaec55c910a291a7c048a75f6805e5c
VegardSkaansar <vegaskaa@stud.ntnu.no>
2018-10-24 20:10:53 +0200

    adding handlerTime function, will handle paraglider/api

--- paragliderdb/paragliderReq.go
+++ paragliderdb/paragliderReq.go
@@ -9,7 +9,7 @@ import (
 // And sends the requests where it should be
 func RootHandler(w http.ResponseWriter, r *http.Request) {
 	parts := strings.Split(r.URL.Path, "/")
-	if parts[1] != "paraglider" && len(parts) <= 2 {
+	if parts[1] != "paraglider" || len(parts) <= 2 {
 		http.Error(w, http.StatusText(404), 404)
 		return
 	} else {
