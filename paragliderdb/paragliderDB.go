a09bf25460f2fb07baaeb6764d8f54cbc3f708e6
VegardSkaansar <vegaskaa@stud.ntnu.no>
2018-10-26 18:03:17 +0200

    made a interface for mongodb and made a add and init func, + added some structures for get and post requests

--- paragliderdb/paragliderDB.go
+++ paragliderdb/paragliderDB.go
@@ -1,6 +1,11 @@
 package paragliderdb
 
-import "time"
+import (
+	"fmt"
+	"time"
+
+	"gopkg.in/mgo.v2"
+)
 
 // ------------------------------------------------------------------------
 // -                 -- GLOBAL Datastructures --                          -
@@ -23,6 +28,11 @@ type TrackMeta struct {
 	TrackSrcURL string  `json:"track_src_url"`
 }
 
+// TrackURL structure for json body when posting
+type TrackURL struct {
+	URL string `json:"url"`
+}
+
 // ------------------------------------------------------------------------
 // -                 -- GLOBAL Variables --                               -
 //-------------------------------------------------------------------------
@@ -30,3 +40,63 @@ type TrackMeta struct {
 // StartTime global variable that will be intialised and sets the time from
 // when the server starts
 var StartTime time.Time
+
+// ------------------------------------------------------------------------
+// -                 -- MongoDB --                                        -
+//-------------------------------------------------------------------------
+
+// TrackerDB is the interface we use to navigate our database
+type TrackerDB interface {
+	Init()
+	AddURL(url TrackURL) error
+	GetID()
+	GetAll()
+}
+
+// MongoDB here we store the information about connection
+type MongoDB struct {
+	DatabaseURL    string
+	DatabaseName   string
+	CollectionName string
+}
+
+// Init function initilises the mongodb
+func (db *MongoDB) Init() {
+	session, err := mgo.Dial(db.DatabaseURL)
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	index := mgo.Index{
+		Key:        []string{"url"},
+		Unique:     true,
+		DropDups:   true,
+		Background: true,
+		Sparse:     true,
+	}
+
+	err = session.DB(db.DatabaseName).C(db.CollectionName).EnsureIndex(index)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+// AddURL adds a new URL to the database (mongodb)
+func (db *MongoDB) AddURL(url TrackURL) error {
+	session, err := mgo.Dial(db.DatabaseURL)
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	err = session.DB(db.DatabaseName).C(db.CollectionName).Insert(url)
+
+	if err != nil {
+		fmt.Printf("Somethings wrong with Insert():%v", err.Error())
+		return err
+	}
+
+	return nil
+}
