22976342b666c3892c7ed30df7eb98acf846e0e4
VegardSkaansar <vegaskaa@stud.ntnu.no>
2018-11-05 22:34:55 +0100

    fixing typos and go vet

--- paragliderdb/paragliderWebhooks.go
+++ paragliderdb/paragliderWebhooks.go
@@ -7,8 +7,8 @@ import (
 // Webhookinfo handles a webhook request
 type Webhookinfo struct {
 	ID                 bson.ObjectId `bson:"_id,omitempty" json:"-"`
-	URL                string        `json:"webhook_URL"`
-	MinTriggerValue    int           `json:"min_trigger_value" bson:"min_trigger_value"`
+	URL                string        `json:"webhookURL"`
+	MinTriggerValue    int           `json:"minTriggerValue" bson:"min_trigger_value"`
 	LatestInvokedTrack bson.ObjectId `bson:"latest_invoked_track" json:"-"`
 	WebhookID          string        `bson:"webhookid" json:"webhookid"`
 }
